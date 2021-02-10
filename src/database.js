54d6381aed0515941a0205863194e3202216fdaf
= <easo02@hotmail.fr>
2021-02-10 12:10:43 -0500

    back to connection sql

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql')
 
-const mySqlConnection = mysql.createPool({
+const mySqlConnection = mysql.createConnection({
     host:process.env.HOST_DB,
     user:process.env.USER_DB,
     password:process.env.PASSWORD_DB,
