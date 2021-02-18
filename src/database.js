6464dde81f688b845cf0ffd7f1d079ffe4eb2e5b
easo02@hotmail.fr <easo02@hotmail.fr>
2021-02-18 12:11:18 -0500

    change pool connect method db

--- src/database.js
+++ src/database.js
@@ -1,6 +1,14 @@
 const mysql = require('mysql')
 
-const mySqlConnection = mysql.createConnection({
+/*const mySqlConnection = mysql.createConnection({
+    host:process.env.HOST_DB,
+    user:process.env.USER_DB,
+    password:process.env.PASSWORD_DB,
+    port:process.env.PORT_DB,
+    database:process.env.DATABASE_DB
+})*/
+
+var mySqlConnection = mysql.createPool({
     host:process.env.HOST_DB,
     user:process.env.USER_DB,
     password:process.env.PASSWORD_DB,
@@ -8,13 +16,26 @@ const mySqlConnection = mysql.createConnection({
     database:process.env.DATABASE_DB
 })
 
-mySqlConnection.connect((err) =>{
+// Attempt to catch disconnects 
+mySqlConnection.on('connection', function (connection) {
+    console.log('DB Connection established')
+
+    connection.on('error', function (err) {
+        console.error(new Date(), 'MySQL error', err.code);
+    })
+    connection.on('close', function (err) {
+        console.error(new Date(), 'MySQL close', err);
+    })
+
+})
+
+/*mySqlConnection.connect((err) =>{
     if(err){
         console.log(err)
         return
     }else{
         console.log('database connected')
     }
-})
+})*/
 
 module.exports = mySqlConnection
