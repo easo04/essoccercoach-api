5e20048ab76bae2fb420ea539e386a55603c0b71
easo02@hotmail.fr <easo02@hotmail.fr>
2021-06-16 16:50:05 -0400

    modif route name

--- src/routes/availabilities.js
+++ src/routes/availabilities.js
@@ -8,7 +8,7 @@ const availabilityController = require('../controllers/availabilityController');
 router.post('/', authorize(), availabilityController.add_availability)
 router.post('/update', authorize(), availabilityController.update_availability)
 router.get('/:id', authorize(), availabilityController.get_availability_by_id)
-router.get('/get-availabilities-by-team/:id', authorize(), availabilityController.get_availabilities_by_activity)
+router.get('/get-availabilities-by-activity/:id', authorize(), availabilityController.get_availabilities_by_activity)
 router.delete('/:id', authorize(), availabilityController.delete_availability)
 
 
